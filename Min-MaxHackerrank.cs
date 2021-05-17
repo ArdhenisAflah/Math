556e026ba498967f3be1418f0d9a3963304b8b37
Ardhenis Muhammad Aflah <aflahdenis9@gmail.com>
2021-05-17 12:50:29 +0700

    Create Min-MaxHackerrank.cs

---
+++ Min-MaxHackerrank.cs
@@ -0,0 +1,46 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long temp = 0;
+        for(int x =0;x < arr.Count(); x++){
+            temp += arr[x];
+        }
+        Console.Write(temp - arr.Max()+ " ");
+        Console.Write(temp -arr.Min());
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
