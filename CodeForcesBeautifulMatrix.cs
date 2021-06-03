33cc3e4d4f810f47c72719603c1b5b2a238786d7
Ardhenis Muhammad Aflah <aflahdenis9@gmail.com>
2021-06-03 13:11:50 +0700

    Update CodeForcesBeautifulMatrix.cs

--- CodeForcesBeautifulMatrix.cs
+++ CodeForcesBeautifulMatrix.cs
@@ -2,78 +2,12 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
-using System.Runtime.InteropServices;
-using System.IO;
-using System.Text.RegularExpressions;
-
 namespace HackerrankSolutionList
 {
     class Program
     {
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        static extern bool DefineDosDeviceW(uint dwFlags, string lpDeviceName, string lpTargetPath);
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        static extern bool MoveFileExW(string file, string newfile, uint dwflag);
-
-        private const string dosdevname = "RIPlace";
-
-        private const uint DDD_REMOVE_DEFINITION = 0x00000002;
-        private const uint DDD_RAW_TARGET_PATH = 0x00000001;
-        private const uint MOVEFILE_REPLACE_EXISTING = 0x00000001;
-        private const uint MOVEFILE_WRITE_THROUGH = 0x00000008;
-        private const string dosdevpath = @"\\.\" + dosdevname;
-
-
-
-        static void Cleanup(string filedelete = null)
-        {
-
-            DefineDosDeviceW(DDD_REMOVE_DEFINITION | DDD_RAW_TARGET_PATH, dosdevname, string.Empty);
-            try
-            {
-                File.Delete(filedelete);
-            }
-            catch
-            {
-
-            }
-        }
-        public static bool RIPlace(string sourcepath, string destination)
-        {
-            //masukkan kode ini ke main function jika diperlukan.
-            const string dummyfile = @"D:\TARGET\JUSTTXT.txt";
-            const string targetfile = @"D:\TARGET\source.txt";
-
-
-            RIPlace(dummyfile, targetfile);
-
-            Console.ReadLine();
-
-            //
-            if (!DefineDosDeviceW(DDD_RAW_TARGET_PATH, dosdevname, @"\??\" + destination)) {
-                Console.WriteLine("pokoknya gabisa udh gausah banyak cincong");
-                return false;
-            } if (!MoveFileExW(sourcepath, dosdevpath, MOVEFILE_REPLACE_EXISTING | MOVEFILE_WRITE_THROUGH))
-            {
-                Console.Write("gabisa");
-                Cleanup(); //THIS IMPORTANT
-                return false;
-            }
-            return true;
-        }
-        //static int MaxToys(List<int> price,int budget)
-        //{
-        //    int max_value = 0;
-        //    Array.Sort(price.ToArray());
-        //    if(price.Count() < 1 || price.Count() < 0)
-        //    {
-        //        return max_value;
-        //    }
-
-
-        //}
-        
+      
         static void Main(string[] args)
         {
             int[,] matrix = new int[5, 5];
@@ -101,7 +35,7 @@ namespace HackerrankSolutionList
             }
             //ini metode cepat(cukup cepat wkwkw) kalau mau lebih cepat tinggal cek pas matrix = 1 copy rows sama column nya trus lakuin kauyak dibawah.
             Console.WriteLine(Math.Abs(rowTarget - rows) + Math.Abs(columnTarget - columns));
-            //ini metode alternatif walaupun mungkin sufficient tetapi ini adlaah metode yang paling sederhana.
+            //ini metode alternatif walaupun mungkin sufficient tetapi ini adlaah metode yang paling sederhana(perlu ditingkatkan sedikit lagi untuk Optimasi).
             int rowChanged = 0;
             int ColumnChanged = 0;
             bool stop1 = false;
@@ -178,34 +112,10 @@ namespace HackerrankSolutionList
                 }
 
             }
-            //for (int x = 0; x < 5; x++)
-            //{
-
-
-            //    for (int c = 0; c < 5; c++)
-            //    {
-
-            //        Console.WriteLine(matrix[x, c]); 
-            //    }
-            //}
-          
+            
             Console.ReadLine();
 
         }
-        static int MaxValue(long result)
-        {
-            //fibonacci DP
-            int[] memo = new int[result + 1];
-            memo[0] = 1;
-            memo[1] = 1;
-            memo[2] = 2;
-            for (int x = 3; x <= result; x++)
-            {
-                memo[x] = memo[x - 1] + memo[x - 2] + memo[x - 3];
-            }
-            return memo.Last();
-
-
-        }
+      
     }
 }
